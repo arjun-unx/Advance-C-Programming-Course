3d6f4cfdaa57e9b65ef7b747f21f82962a4d4654
Arjun Ramesh <84034246+arjun-unx@users.noreply.github.com>
2024-03-01 16:31:00 +0530

    Update Week 1.c

--- Week 1.c
+++ Week 1.c
@@ -46,3 +46,30 @@ int main() {
 
     return 0;
 }
+
+3. 
+
+  #include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1, num2;
+
+    // Input
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Define function pointer and use it to swap
+    void (*swapPtr)(int*, int*) = swap;
+    swapPtr(&num1, &num2);
+
+    // Output
+    printf("Swapped numbers: %d %d\n", num1, num2);
+
+    return 0;
+}
