281a57ce51c9d9117477ee583a1c6d458348053e
Arjun Ramesh <84034246+arjun-unx@users.noreply.github.com>
2024-03-01 16:29:55 +0530

    Update Week 1.c

--- Week 1.c
+++ Week 1.c
@@ -1,1 +1,24 @@
+1. 
+  
+  #include <stdio.h>
 
+int main() {
+    int num;
+
+    // Input
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check the least significant bit using bitwise AND
+    if (num & 1) {
+        printf("%d is odd.\n", num);
+    } else {
+        printf("%d is even.\n", num);
+    }
+
+    return 0;
+}
+
+2.
+
+  
