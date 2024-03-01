406c055fb2589e01d3d8068f9784b15341205bec
Arjun Ramesh <84034246+arjun-unx@users.noreply.github.com>
2024-03-01 16:30:39 +0530

    Update Week 1.c

--- Week 1.c
+++ Week 1.c
@@ -21,4 +21,28 @@ int main() {
 
 2.
 
-  
+  #include <stdio.h>
+
+int countSetBits(int num) {
+    int count = 0;
+
+    while (num) {
+        count += num & 1;
+        num >>= 1;
+    }
+
+    return count;
+}
+
+int main() {
+    int num;
+
+    // Input
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Count set bits
+    printf("Count of Set bits: %d\n", countSetBits(num));
+
+    return 0;
+}
